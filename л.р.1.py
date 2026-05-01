0fc4c896b26306ec9e08b4ba77eb9d20923733a6
pacuk_56 <hannavaitsiankova3@gmail.com>
2026-05-01 19:33:29 +0300

    Сделанная лабораторная работа 1,2

---
+++ л.р.1.py
@@ -0,0 +1,24 @@
+# Лабораторная работа 1, вариант 4
+
+import itertools
+
+try:
+    n = int(input("Введите n (от 1 до 7): "))
+
+    if n > 7:
+        print("Ошибка: n не должно превышать 7")
+    elif n < 1:
+        print("Ошибка: n должно быть натуральным числом")
+    else:
+        # range создает список от 1 до n
+        numbers = list(range(1, n + 1))
+        # itertools.permutations возвращает итератор со всеми вариантами
+        all = list(itertools.permutations(numbers))
+
+        print(len(all))
+        # Выводим каждую перестановку, разделяя числа пробелом
+        for p in all:
+            print(*p)
+
+except ValueError:
+    print("Ошибка: введите целое число")
